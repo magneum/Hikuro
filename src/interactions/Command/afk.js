372542ae209408504daece47dd7c48d6346fa671
ӄʀǟӄɨռʐӄօռ <krakinzkon@gmail.com>
2023-06-21 14:42:52 +0000

    Update 8 files

--- src/interactions/Command/afk.js
+++ src/interactions/Command/afk.js
@@ -1,41 +1,34 @@
-const { CommandInteraction, Client } = require('discord.js');
-const { SlashCommandBuilder } = require('discord.js');
-const Discord = require('discord.js');
-
-const Schema = require("../../database/models/music");
+const { CommandInteraction, Client } = require("discord.js");
+const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('afk')
-        .setDescription('Set your AFK')
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('help')
-                .setDescription('Get information about the afk category commands')
+  data: new SlashCommandBuilder()
+    .setName("afk")
+    .setDescription("Set your AFK")
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("help")
+        .setDescription("Get information about the afk category commands")
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("set")
+        .setDescription("Put yourself AFK")
+        .addStringOption((option) =>
+          option.setName("reason").setDescription("The reason for your AFK")
         )
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('set')
-                .setDescription('Put yourself AFK')
-                .addStringOption(option => option.setName('reason').setDescription('The reason for your AFK'))
-        )
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('list')
-                .setDescription('Show all afk users')
-        )
-    ,
+    )
+    .addSubcommand((subcommand) =>
+      subcommand.setName("list").setDescription("Show all afk users")
+    ),
+  /**
+   * @param {Client} client
+   * @param {CommandInteraction} interaction
+   * @param {String[]} args
+   */
 
-    /** 
-     * @param {Client} client
-     * @param {CommandInteraction} interaction
-     * @param {String[]} args
-     */
-
-    run: async (client, interaction, args) => {
-        await interaction.deferReply({ fetchReply: true });
-        client.loadSubcommands(client, interaction, args);
-    },
+  run: async (client, interaction, args) => {
+    await interaction.deferReply({ fetchReply: true });
+    client.loadSubcommands(client, interaction, args);
+  },
 };
-
- 
